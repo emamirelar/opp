c9c240e081fd07fece2f06294dfebbd6e0170934
Tafazzul Mohammed <tafazzulm@unops.org>
2025-03-04 18:33:55 -0500

    Entity partner with CRUD operations with UI enhancements

--- UNOPS.PAO.UNOPSBusiness/Managers/UNOPSManagerWrapper.cs
+++ UNOPS.PAO.UNOPSBusiness/Managers/UNOPSManagerWrapper.cs
@@ -15,6 +15,7 @@ public class UNOPSManagerWrapper : ManagerWrapper
     private readonly DocumentManager documentManager;
     private readonly UNOPSSystemAdminManager systemAdminManager;
     private readonly UNOPSContactManager contactManager;
+    private readonly UNOPSPartnerManager partnerManager;
 
     public UNOPSManagerWrapper(IMapper mapper, AppDbContext context, UNOPSAppDbContext opsContext, IGoogleDriveDocumentManager driveManager, IConfiguration configuration) : base(mapper, context)
     {
@@ -22,10 +23,12 @@ public class UNOPSManagerWrapper : ManagerWrapper
         documentManager = new DocumentManager(driveManager, configuration, mapper, opsContext);
         systemAdminManager = new UNOPSSystemAdminManager(opsContext);
         contactManager = new UNOPSContactManager(mapper, opsContext);
+        partnerManager = new UNOPSPartnerManager(mapper,opsContext);
     }
 
     public override IFundingOpportunityManager FundingOpportunityManager => fundingOpportunityManager;
     public override IDocumentManager DocumentManager => documentManager;
     public override ISystemAdminManager SystemAdminManager => systemAdminManager;
     public override IContactManager ContactManager => contactManager;
+    public override IPartnerManager PartnerManager => partnerManager;
 }
