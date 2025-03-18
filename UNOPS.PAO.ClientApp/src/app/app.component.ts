c61e89354a3c831749a2db9a214c66e599657440
laurent <eloua>
2025-03-18 17:00:13 +0100

    PNO-52 Connect AI Assistant to the back

--- UNOPS.PAO.ClientApp/src/app/app.component.ts
+++ UNOPS.PAO.ClientApp/src/app/app.component.ts
@@ -3,14 +3,16 @@ import { RouterModule, RouterOutlet } from '@angular/router';
 import { AuthService } from './essentials/services/auth.service';
 import { ToastModule } from 'primeng/toast';
 import { FeedbackDialogComponent } from './common/reusables/widgets/feedback-dialog/feedback-dialog.component';
-import { AiAssistantDialogComponent } from './common/reusables/widgets/ai-assistant-dialog/ai-assistant-dialog.component';
+import { AiAssistantComponent } from './common/reusables/widgets/ai-assistant/ai-assistant.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterModule, ToastModule, FeedbackDialogComponent, AiAssistantDialogComponent],
+  imports: [RouterOutlet, RouterModule, ToastModule, FeedbackDialogComponent, AiAssistantComponent],
   template: `
   <app-feedback-dialog></app-feedback-dialog>
-  <app-ai-assistant-dialog></app-ai-assistant-dialog>
+  @if(isLoggedIn) {
+    <app-ai-assistant></app-ai-assistant>
+  }
   <router-outlet></router-outlet>`,
   standalone: true,
 })
