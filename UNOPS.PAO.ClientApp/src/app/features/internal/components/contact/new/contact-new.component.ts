ae1f9a48517bcd5e9154247a8ee4bcb9cbcefe5b
AnushaSwami <anushas@unops.org>
2025-03-19 10:23:02 +0100

    Navigate to the created contact page

--- UNOPS.PAO.ClientApp/src/app/features/internal/components/contact/new/contact-new.component.ts
+++ UNOPS.PAO.ClientApp/src/app/features/internal/components/contact/new/contact-new.component.ts
@@ -23,6 +23,8 @@ import { ContactService } from '../../../services/contact.service';
 import { CardModule } from 'primeng/card';
 import { DialogModule } from 'primeng/dialog';
 
+import { Router } from '@angular/router';
+
 @Component({
   selector: 'app-contact-new',
   imports: [
@@ -48,6 +50,7 @@ import { DialogModule } from 'primeng/dialog';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ContactNewComponent implements OnInit, OnDestroy, OnChanges {
+  router = inject(Router);
   public formGroup = new FormGroup({
     // Basic contact information
     salutation: new FormControl('', { validators: [Validators.required] }),
@@ -151,7 +154,11 @@ export class ContactNewComponent implements OnInit, OnDestroy, OnChanges {
       this.contactService.createContact(this._getRequestPayload()).subscribe({
         next: (data: any) => {
           this.feedbackDialogService.showSuccessToast({ detail: 'Record created successfully!' });
-          this.onRecordCreationSuccess.emit(data);
+          if (Object.keys(this.record).length > 0) {
+              this.router.navigate([ 'contact', data.id ]);
+          } else {
+              this.onRecordCreationSuccess.emit(data);
+          }
           this.hide();
         }
       });
