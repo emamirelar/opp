b94b2f1dd7b2e21d8e1bab3d82c3fe0e712457d7
Tafazzul Mohammed <tafazzulm@unops.org>
2025-08-20 08:13:34 -0400

    Fix for contact creation issue

--- UNOPS.PAO.Business/Repositories/ValuesRepository.cs
+++ UNOPS.PAO.Business/Repositories/ValuesRepository.cs
@@ -24,7 +24,7 @@ public class ValuesRepository
 
     public IQueryable<Partner> GetPartners()
         => context.Partners
-            .Where(x => x.Status.Equals("Active") && !x.IsDeleted);
+            .Where(x => x.Status == EntityStatus.Active && !x.IsDeleted);
     
     // Get flat list of organization units by type
     public IEnumerable<OrganizationHierarchy> GetOrganizationsByType(OrganizationUnitType type)
