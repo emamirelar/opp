63854997ef7d29a0bb58e3fac9227f077f05d97c
AnushaSwami <anushas@unops.org>
2025-09-10 11:42:13 +0200

    Fixing partner engagement issue

--- UNOPS.PAO.UNOPSDataAccess/Migrations/UNOPSAppDbContextModelSnapshot.cs
+++ UNOPS.PAO.UNOPSDataAccess/Migrations/UNOPSAppDbContextModelSnapshot.cs
@@ -1233,7 +1233,7 @@ namespace UNOPS.PAO.UNOPSDataAccess.Migrations
                     b.Property<int?>("DueDiligenceRequired")
                         .HasColumnType("integer");
 
-                    b.Property<int>("ErpDimValue")
+                    b.Property<int?>("ErpDimValue")
                         .HasColumnType("integer");
 
                     b.Property<bool>("IsDeleted")
