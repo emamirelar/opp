832f79ac7151f8656ccc898c07acf6c2bc8c4762
AnushaSwami <anushas@unops.org>
2025-09-12 20:41:00 +0200

    SeedScripts

--- UNOPS.PAO.UNOPSDataAccess/Context/UNOPSAppDbContext.cs
+++ UNOPS.PAO.UNOPSDataAccess/Context/UNOPSAppDbContext.cs
@@ -186,4 +186,7 @@ public class UNOPSAppDbContext : AppDbContext
     
     // AI-related DbSets
     public DbSet<AiPrompt> AiPrompts { get; set; }
+    
+    // Seed script tracking
+    public DbSet<SeedScript> SeedScripts { get; set; }
 }
--- UNOPS.PAO.UNOPSDataAccess/Migrations/UNOPSAppDbContextModelSnapshot.cs
+++ UNOPS.PAO.UNOPSDataAccess/Migrations/UNOPSAppDbContextModelSnapshot.cs
@@ -2146,6 +2146,47 @@ namespace UNOPS.PAO.UNOPSDataAccess.Migrations
                     b.ToTable("EntityManagers", "public");
                 });
 
+            modelBuilder.Entity("UNOPS.PAO.UNOPSDomain.Entities.SeedScript", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("integer");
+
+                    NpgsqlPropertyBuilderExtensions.UseIdentityByDefaultColumn(b.Property<int>("Id"));
+
+                    b.Property<string>("Description")
+                        .HasColumnType("text");
+
+                    b.Property<int>("ExecutionOrder")
+                        .HasColumnType("integer");
+
+                    b.Property<string>("FileHash")
+                        .IsRequired()
+                        .HasColumnType("text");
+
+                    b.Property<DateTime>("LastExecutedDate")
+                        .HasColumnType("timestamp with time zone");
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasColumnType("text");
+
+                    b.Property<string>("ScriptName")
+                        .IsRequired()
+                        .HasColumnType("text");
+
+                    b.Property<string>("ScriptType")
+                        .IsRequired()
+                        .HasColumnType("text");
+
+                    b.Property<int>("Status")
+                        .HasColumnType("integer");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("SeedScripts", "public");
+                });
+
             modelBuilder.Entity("UNOPS.PAO.UNOPSDomain.Entities.UNOPSContact", b =>
                 {
                     b.HasBaseType("UNOPS.PAO.Domain.Entities.Contact");
@@ -2274,7 +2315,6 @@ namespace UNOPS.PAO.UNOPSDataAccess.Migrations
                     b.HasOne("UNOPS.PAO.Domain.Entities.Partner", "Partner")
                         .WithMany()
                         .HasForeignKey("PartnerId")
-                        .HasPrincipalKey("ErpDimValue")
                         .OnDelete(DeleteBehavior.SetNull);
 
                     b.Navigation("Partner");
