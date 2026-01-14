88dff8b71e88d2b98a6e1902c54f76c29a7269b1
AnushaSwami <anushas@unops.org>
2026-01-14 13:27:42 +0100

    Strict length restructions for fields in opportunity

---
+++ UNOPS.PAO.UNOPSDataAccess/Migrations/20260114122208_UpdateOpportunityFieldLength.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UNOPS.PAO.UNOPSDataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateOpportunityFieldLength : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "public",
+                table: "Opportunities",
+                type: "character varying(120)",
+                maxLength: 120,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(255)",
+                oldMaxLength: 255);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Challenges",
+                schema: "public",
+                table: "Opportunities",
+                type: "character varying(1020)",
+                maxLength: 1020,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                schema: "public",
+                table: "Opportunities",
+                type: "character varying(255)",
+                maxLength: 255,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(120)",
+                oldMaxLength: 120);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Challenges",
+                schema: "public",
+                table: "Opportunities",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(1020)",
+                oldMaxLength: 1020,
+                oldNullable: true);
+        }
+    }
+}
